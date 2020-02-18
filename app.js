64a50401af10d62e9a60e15964ce477861525880
Brad Roodt <brad.roodt@derivco.com>
2020-02-18 14:15:54 +0200

    Basic working CRUD methods with internal store

--- app.js
+++ app.js
@@ -1,9 +1,56 @@
+const uuidv4 = require('uuid/v4');
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const port = 3123;
-app.get('/posts', (req, res)=> res.send('Some posts'));
+const port = 3203;
+
+app.use(bodyParser.json());
+
+const posts = [
+  {
+    title:'Post1',
+    content: 'Post 1 Content'   ,
+    id: uuidv4() 
+  },
+  {
+    title:'Post2',
+    content: 'Post 2 Content',
+    id: uuidv4() 
+  },
+  {
+    title:'Post3',
+    content: 'Post 3 Content',
+    id: uuidv4()
+  }
+];
+
+//CREATE
+app.post('/posts', (req, res)=> {
+  const post = req.body;
+  post.id = uuidv4();
+  posts.push(post);    
+  return res.status(200).json(posts[posts.length-1]);
+});
+
+//READ
+app.get('/posts', (req, res)=> res.status(200).json(posts));
+app.get('/post/:id', (req, res)=> res.status(200).json(posts[req.params.id]));
+app.put('/post/:id', (req, res) => { 
+  const updatedPost = req.body;  
+  const postIndex = posts.findIndex(post => post.id === req.params.id);   
+  if (postIndex == -1) return res.status(500).send({error: "Post not found"}); 
+  posts[postIndex] = updatedPost;
+  return res.status(200).json(posts[postIndex]);
+});  
+
+app.delete('/post/:id', (req, res) => {  
+  const postIndex = posts.findIndex(post => post.id === req.params.id);   
+  if (postIndex == -1) return res.status(500).send({error: "Post not found"});  
+  const deletedPost = posts.splice(postIndex, 1);  
+  return res.status(200).json(deletedPost[0]);
+});
 
 app.listen(port, ()=> console.log(`listening on ${port}`));
 
-module.export = app;
+module.exports = app;
 
--- app.spec.js
+++ app.spec.js
@@ -1,10 +1,190 @@
-const request = require('supertest');
+const supertest = require('supertest');
 const app = require('./app');
+const req = supertest(app);
 
-describe('App', async ()=> {
-  it('should get posts', () => {
-    const response= await.get('posts')
-    .expects(200);
-    expect(response.body).toBe('Bob');
+describe('GET /posts', ()=> {
+  it('should have status code of 200', async () => {
+    const res = await req.get('/posts');
+    expect(res.status).toBe(200);
+  });
+
+  it ('should return an array of posts', async() => {
+    const res = await req.get('/posts');
+    expect(Array.isArray(res.body)).toBe(true);
+  });
+
+  it ('post should have properties, title & content', async() => {    
+    const res = await req.get('/posts');
+    const postID = Math.floor(Math.random() * res.body.length);
+    expect(res.body[postID]).toHaveProperty('title');
+    expect(res.body[postID]).toHaveProperty('content');
   })
-})
+});
+
+describe('GET post/:id', () => {
+  it('should return status code of 200', async () => {
+    const res = await req.get('/post/0');
+    expect(res.status).toBe(200);
+  });
+
+  it('should return an object', async () => {
+    const res = await req.get('/post/0');
+    expect(typeof res.body).toBe('object');
+  });
+
+  it('retrieved post should have title and content properties', async ()=> {
+    const res = await req.get('/post/0');    
+    expect(res.body).toHaveProperty('title');
+    expect(res.body).toHaveProperty('content');
+  });
+});
+
+
+describe('POST /posts', () => {
+  it ('should create a post', async ()=> {
+    const res = await req.post('/posts')
+    .send({
+      title: 'New post',
+      content: 'New post content'
+    })
+    .set('Accept', 'application/json')    
+    
+
+    expect(res.status).toBe(200);
+  });
+
+  it ('should return the post that was created', async ()=> {
+    const userPost = {
+      title: 'New post created',
+      content: 'New post content created'
+    }
+    const res = await req.post('/posts')
+    .send(userPost)
+    .set('Accept', 'application/json')   
+    
+
+    expect(res.body).toHaveProperty('title');
+    expect(res.body).toHaveProperty('content');
+    expect(res.body.title).toBe(userPost.title);
+    expect(res.body.content).toBe(userPost.content);
+  });
+});
+
+describe('PUT /post/:id', () => {
+  it ('should update a post and return status code of 200 if post exists', async ()=> {
+    const userPost = {
+      title: 'New post created',
+      content: 'New post content created'
+    };   
+
+    const initialPostRes = await req.post('/posts')
+    .send(userPost)
+    .set('Accept', 'application/json');        
+    
+    const updatedUserPost =  Object.assign(initialPostRes.body, {
+      title: 'Post title updated',
+      content: 'Post content updated'
+    });       
+    const res = await req.put(`/post/${updatedUserPost.id}`)    
+    .send(updatedUserPost)
+    .set('Accept', 'application/json');        
+    expect(res.status).toBe(200);    
+
+  });
+
+  it ('should update a post and return updated post', async ()=> {
+    const userPost = {
+      title: 'New post created',
+      content: 'New post content created'
+    };   
+
+    const initialPostRes = await req.post('/posts')
+    .send(userPost)
+    .set('Accept', 'application/json');        
+    
+    const updatedUserPost =  Object.assign(initialPostRes.body, {
+      title: 'Post title updated',
+      content: 'Post content updated'
+    });       
+    const res = await req.put(`/post/${updatedUserPost.id}`)    
+    .send(updatedUserPost)
+    .set('Accept', 'application/json');  
+    const returnedPost = res.body;
+    expect(returnedPost).toHaveProperty('title');
+    expect(returnedPost).toHaveProperty('content');  
+    expect(returnedPost).toHaveProperty('id');
+    expect(returnedPost.title).toBe(updatedUserPost.title);  
+    expect(returnedPost.content).toBe(updatedUserPost.content);  
+    expect(returnedPost.id).toBe(initialPostRes.body.id);  
+
+  });
+
+   it ('should return status code of 500 if attempting to update non-existant post', async ()=> {
+    const userPost = {
+      title: 'New post created',
+      content: 'New post content created',
+      id: '234234134'
+    };   
+    
+    const res = await req.put(`/post/-1`)
+    .send(userPost)
+    .set('Accept', 'application/json');        
+
+    expect(res.status).toBe(500);    
+  });
+
+  it ('should return error if attempting to update non-existant post', async ()=> {
+    const userPost = {
+      title: 'New post created',
+      content: 'New post content created',
+      id: '234234134'
+    };   
+    
+    const res = await req.put(`/post/-1`)
+    .send(userPost)
+    .set('Accept', 'application/json');        
+
+    expect(res.body).toHaveProperty('error');        
+  });
+ 
+});
+
+describe('DELETE post/:id', () => {
+  it('should delete an existing post', async ()=> {
+    const postsRes = await req.get('/posts');
+    const posts = postsRes.body;
+    const randomPostIndex= Math.floor(Math.random() * posts.length);
+    const randomPost = posts[randomPostIndex];
+    
+    const res = await req.delete(`/post/${randomPost.id}`);
+    expect(res.status).toBe(200);
+  });
+
+  it('should delete an existing post', async ()=> {
+    const postsRes = await req.get('/posts');
+    const posts = postsRes.body;
+    const randomPostIndex= Math.floor(Math.random() * posts.length);
+    const randomPost = posts[randomPostIndex];
+    
+    const res = await req.delete(`/post/${randomPost.id}`);
+    expect(res.status).toBe(200);
+  });
+
+  it('should return the deleted post', async ()=> {
+    const postsRes = await req.get('/posts');
+    const posts = postsRes.body;
+    const randomPostIndex= Math.floor(Math.random() * posts.length);
+    const randomPost = posts[randomPostIndex];
+    
+    const res = await req.delete(`/post/${randomPost.id}`);
+    const returnedPost = res.body;
+    expect(returnedPost).toHaveProperty('title');
+    expect(returnedPost).toHaveProperty('content');  
+    expect(returnedPost).toHaveProperty('id');
+    expect(returnedPost.title).toBe(randomPost.title);  
+    expect(returnedPost.content).toBe(randomPost.content);  
+    expect(returnedPost.id).toBe(randomPost.id);
+    
+  });
+});
+
